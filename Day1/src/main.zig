5029fb4fb93c7ac44734395f2d8fd7f73a7c32f0
Marius Urban <urbanmarius96@yahoo.de>
2023-12-01 09:57:50 +0100

    finshed day 1 part 1

--- Day1/src/main.zig
+++ Day1/src/main.zig
@@ -1,11 +1,34 @@
 const std = @import("std");
 
-const data  = @embedFile("input.txt");
-
 pub fn main() !void {
-    var lines = std.mem.tokenizeAny(u8, &data, "\n");
+    std.debug.print("part1: {}", .{part1(@embedFile("./input.txt"))});
+}
 
-    for(lines) |line| {
+fn part1(input: []const u8) usize {
+    var lines = std.mem.tokenizeAny(u8, input, "\n");
+    var sum: usize = 0;
+
+    while (lines.next()) |line| {
+        var first: usize = 0;
+        var last: usize = 0;
+        var i: usize = 0;
+        inner: while (i < line.len) : (i += 1) {
+            if (std.ascii.isDigit(line[i])) {
+                first = line[i] - '0';
+
+                break :inner;
+            }
+        }
+        for (line) |chr| {
+            if (std.ascii.isDigit(chr)) {
+                last = chr - '0';
+            }
+        }
+        sum += first * 10 + last;
+    }
+    return sum;
+}
+
         _ = line;
 
     }
