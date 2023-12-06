ff7b00cb67f775c2f941cda21b7b339a7488fa40
Marius Urban <urbanmarius96@yahoo.de>
2023-12-06 21:50:01 +0100

    finished Day 6

--- Day6/src/main.zig
+++ Day6/src/main.zig
@@ -78,9 +78,59 @@ test "p1" {
     try std.testing.expectEqual(expected, actual);
 }
 
-fn part2(input: []u8) usize {
-    _ = input;
-    const score: usize = 0;
+fn part2(input: []const u8) usize {
+    var score: usize = 1;
+    var lines = std.mem.splitAny(u8, input, "\n");
+    var duration = lines.next().?;
+    var distance = lines.next().?;
+
+    //find first difit of
+    var after_header = std.mem.indexOfAny(u8, duration, "0123456789").?;
+    var long_dur: usize = 0;
+    var numbers = std.mem.splitScalar(u8, duration[after_header..], ' ');
+    while (numbers.next()) |num| {
+        if (num.len != 0) {
+            const parsed = std.fmt.parseUnsigned(usize, num, 10) catch unreachable;
+            if (long_dur == 0) {
+                long_dur += parsed;
+            } else {
+                long_dur = std.math.pow(usize, 10, num.len) * long_dur + parsed;
+            }
+        }
+    }
+    //find distances
+    after_header = std.mem.indexOfAny(u8, distance, "0123456789").?;
+    var long_dist: usize = 0;
+    numbers = std.mem.splitScalar(u8, distance[after_header..], ' ');
+    while (numbers.next()) |num| {
+        if (num.len != 0) {
+            const parsed = std.fmt.parseUnsigned(usize, num, 10) catch unreachable;
+            if (long_dist == 0) {
+                long_dist += parsed;
+            } else {
+                long_dist = std.math.pow(usize, 10, num.len) * long_dist + parsed;
+            }
+        }
+    }
+    std.debug.print("\ndist: {}\ndur: {}", .{ long_dist, long_dur });
+    for (0..long_dur) |charge_time| {
+        if (charge_time * (long_dur - charge_time) > long_dist) {
+            const winners = (long_dur + 1) - 2 * (charge_time);
+            score *= winners;
+            std.debug.print("\n\n{any}", .{winners});
+            break;
+        }
+    }
 
     return score;
+}
+
+test "p2" {
+    const map =
+        \\Time:      7  15   30
+        \\Distance:  9  40  200
+    ;
+    const actual: usize = part2(map);
+    const expected: usize = 71503;
+    try std.testing.expectEqual(expected, actual);
 }
