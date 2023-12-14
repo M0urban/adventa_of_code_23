cdb6e49e91a896661e236e0895d936d9f47e8be9
Marius Urban <114688603+M0urban@users.noreply.github.com>
2023-12-14 15:22:13 +0100

    almost finished day14 part 1

--- Day14/src/main.zig
+++ Day14/src/main.zig
@@ -17,22 +17,77 @@ pub fn main() !void {
     try reader.readAllArrayList(&buffer, std.math.maxInt(usize));
 
     //run parts
-    std.debug.print("\npart 1: {}", .{part1(buffer.items, alloc)});
+    std.debug.print("\npart 1: {}", .{part1(buffer.items)});
     std.debug.print("\npart 2: {}", .{part2(buffer.items, alloc)});
 }
 
-fn part1(input: []const u8, alloc: std.mem.Allocator) usize {
-    _ = alloc;
+fn sumOfRange(first: usize, last: usize) usize {
+    var sum: usize = 0;
+    var i = first;
+    while (i <= last) : (i += 1) {
+        sum += i;
+    }
+    return sum;
+}
+
+fn part1(input: []const u8) usize {
     var score: usize = 0;
 
-    const line_len_lf = std.mem.indexOfScalar(u8, input, '\n') + 1;
+    const line_len_lf = std.mem.indexOfScalar(u8, input, '\n').? + 1;
+    std.io.getStdOut().writer().print("\ninput_len: {}, line_len: {}", .{ input.len, line_len_lf }) catch unreachable;
     std.debug.assert(input.len % line_len_lf == line_len_lf - 1);
     const line_len = line_len_lf - 1;
-    _ = line_len;
     const line_ammount = (input.len / line_len_lf) + 1;
-    _ = line_ammount;
+    for (0..line_len) |col| {
+        var movable: usize = 0;
+        var start: usize = 0;
+        var line_idx: usize = 0;
+        while (line_idx < line_ammount) : (line_idx += 1) {
+            switch (input[line_idx * line_len_lf + col]) {
+                '.' => {},
+                'O' => {
+                    movable += 1;
+                },
+                '#' => {
+                    if (movable != 0) {
+                        const group_score = sumOfRange(line_ammount - start - movable + 1, line_ammount - start);
+                        score += group_score;
+                        movable = 0;
+                    }
+                    start = line_idx + 1;
+                },
+                else => {
+                    unreachable;
+                },
+            }
+        }
+        //check if there is one final group
+        if (movable != 0) {
+            const group_score = sumOfRange(line_ammount - start - movable + 1, line_ammount - start);
+            score += group_score;
+            movable = 0;
+        }
+    }
 
     return score;
+}
+
+test "p1" {
+    const input =
+        \\O....#....
+        \\O.OO#....#
+        \\.....##...
+        \\OO.#O....O
+        \\.O.....O#.
+        \\O.#..O.#.#
+        \\..O..#O..O
+        \\.......O..
+        \\#....###..
+        \\#OO..#....
+    ;
+    const expected: usize = 136;
+    const actual: usize = part1(input);
+    try std.testing.expectEqual(expected, actual);
 }
 
 fn part2(input: []const u8, alloc: std.mem.Allocator) usize {
