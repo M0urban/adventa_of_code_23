cd15173eb5a96820f9648e2bb11861f4bc4363c9
Marius Urban <urbanmarius96@yahoo.de>
2023-12-15 18:30:51 +0100

    finished part2 imolementation

--- Day15/src/main.zig
+++ Day15/src/main.zig
@@ -52,3 +52,59 @@ test "p1" {
     try std.testing.expectEqual(expected, actual);
 }
 
+fn part2(input: []const u8, alloc: std.mem.Allocator) usize {
+    var score: usize = 0;
+
+    var hashmap = [_]std.ArrayList([3]u8){std.ArrayList([3]u8).init(alloc)} ** 256;
+    defer {
+        for (hashmap) |map| {
+            map.deinit();
+        }
+    }
+    const no_lf = std.mem.trimRight(u8, input, "\r\n");
+    var instructions = std.mem.splitScalar(u8, no_lf, ',');
+    while (instructions.next()) |ins| {
+        const box = hash(0, ins[0..2]);
+        // std.debug.print("\nhash: {}", .{box});
+        if (ins[2] == '=') {
+            //check if label already in box
+            find: for (0..hashmap[box].items.len) |idx| {
+                if (std.mem.eql(u8, hashmap[box].items[idx][0..2], ins[0..2])) {
+                    std.debug.print("\n{any}   {}", .{ hashmap[box].items, ins[3] });
+                    hashmap[box].items[idx][2] = ins[3];
+                    std.debug.print("\n{any}", .{hashmap[box].items});
+                    break :find;
+                }
+            } else {
+                hashmap[box].append(.{ ins[0], ins[1], ins[3] }) catch unreachable;
+            }
+        } else {
+            find: for (0..hashmap[box].items.len) |idx| {
+                if (std.mem.eql(u8, hashmap[box].items[idx][0..2], ins[0..2])) {
+                    // std.debug.print("\n{any}", .{hashmap[box].items});
+                    _ = hashmap[box].orderedRemove(idx);
+                    // std.debug.print("\n{any}", .{hashmap[box].items});
+                    break :find;
+                }
+            }
+        }
+    }
+
+    for (hashmap, 1..) |box, boxnr| {
+        for (box.items, 1..) |item, slot| {
+            std.debug.print("\nitem: {s} slot:{} box: {}", .{ item, slot, boxnr });
+            score += (item[2] - '0') * slot * boxnr;
+        }
+    }
+
+    return score;
+}
+
+test "p2" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    const input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+    const expected: usize = 145;
+    const actual: usize = part2(input, alloc);
+    try std.testing.expectEqual(expected, actual);
+}
